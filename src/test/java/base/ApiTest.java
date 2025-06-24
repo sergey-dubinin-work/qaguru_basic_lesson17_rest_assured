be0ecc5a281244c279dd4c256e5b97615fd410dc
Sergey Dubinin <s.dubinin@dex-it.ru>
2025-06-24 14:33:25 +0300

    add allure listener

--- src/test/java/base/ApiTest.java
+++ src/test/java/base/ApiTest.java
@@ -1,4 +1,13 @@
 package base;
 
+import io.qameta.allure.restassured.AllureRestAssured;
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeAll;
+
 public abstract class ApiTest {
+
+    @BeforeAll
+    static void beforeAll() {
+        RestAssured.filters(new AllureRestAssured());
+    }
 }
