2c622053a88b730bea9f59c54f12f1572839adfd
Sergey Dubinin <s.dubinin@dex-it.ru>
2025-06-24 14:48:52 +0300

    add allure listener with custom templates

--- src/test/java/base/ApiTest.java
+++ src/test/java/base/ApiTest.java
@@ -1,5 +1,6 @@
 package base;
 
+import helpers.CustomAllureListener;
 import io.qameta.allure.restassured.AllureRestAssured;
 import io.restassured.RestAssured;
 import org.junit.jupiter.api.BeforeAll;
@@ -8,6 +9,7 @@ public abstract class ApiTest {
 
     @BeforeAll
     static void beforeAll() {
-        RestAssured.filters(new AllureRestAssured());
+//        RestAssured.filters(new AllureRestAssured());
+        RestAssured.filters(CustomAllureListener.withCustomTemplates());
     }
 }
